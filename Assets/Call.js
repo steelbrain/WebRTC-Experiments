43fd6bbd19b9c32e5c334aee52dd00bad00f3db0
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-22 14:53:35 -0700

    :new: Add WebRTC Call

---
+++ Assets/Call.js
@@ -0,0 +1,71 @@
+"use strict";
+class WebRTCCall extends WebRTC{
+  constructor(){
+    super();
+    let Me = this;
+    Me.MediaStatus = false;
+    Me.MediaStream = null;
+    this.on('icecandidate', function(Event){
+      if(Event.candidate){
+        Me.emit('candidate', {
+          type: 'Candidate',
+          label: Event.candidate.sdpMLineIndex,
+          id: Event.candidate.sdpMid,
+          candidate: Event.candidate.candidate
+        });
+      }
+    });
+  }
+  Call(Audio, Video){
+    let Me = this;
+    return new Promise(function(Resolve, Reject){
+      let MediaPromise;
+      if(Me.MediaStatus){
+        MediaPromise = Promise.resolve(Me.MediaStream);
+      } else {
+        MediaPromise = Me.addMedia({audio: Boolean(Audio), video: Boolean(Video)});
+      }
+      MediaPromise.then(function(Stream){
+        Me.createOffer().then(function(Offer){
+          Offer = Offer.toJSON();
+          Offer.Audio = Audio;
+          Offer.Video = Video;
+          Resolve({Offer: Offer, Stream: Stream});
+        }, Reject);
+      }, Reject);
+    });
+  }
+  OnCandidate(Message){
+    if(this.MediaStatus)
+      this.addCandidate(new RTCIceCandidate({sdpMLineIndex: Message.label,candidate: Message.candidate}));
+  }
+  OnOffer(Offer){
+    let Me = this;
+    return new Promise(function(Resolve, Reject){
+      let MediaPromise;
+      if(Me.MediaStatus){
+        MediaPromise = Promise.resolve(Me.MediaStream);
+      } else {
+        MediaPromise = Me.addMedia({audio: Boolean(Offer.Audio), video: Boolean(Offer.Video)});
+      }
+      MediaPromise.then(function(Stream){
+        Me.setRemote(Offer).then(function(){
+          Me.createAnswer().then(function(Answer){
+            Resolve({Answer: Answer, Stream: Stream});
+          });
+        });
+      }, Reject);
+    });
+  }
+  addMedia(Constraints){
+    let DeSuper = super.addMedia.bind(this);
+    let Me = this;
+    return new Promise(function(Resolve, Reject){
+      DeSuper(Constraints).then(function(Stream){
+        Me.MediaStream = Stream;
+        Me.MediaStatus = true;
+        Resolve(Stream);
+      }, Reject);
+    });
+  }
+}
